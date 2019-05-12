bdb235590db78661b1140b32f9b9dd6e6706988d
ysygitee <yaosiyuanyouxiang@163.com>
2019-05-12 22:25:33 +0800

    first commit

---
+++ MsUserVo.java
@@ -0,0 +1,39 @@
+/**
+ * @description: 功能描述：()
+ * @copyright: Copyright (c) 2019
+ * @company: 昭阳科技
+ * @author: 
+ * @version: 2.0
+ * @date: 2019 2019年5月3日 下午8:30:20
+*/
+package com.yaosiyuan.vo;
+
+/**
+ * @description: 功能描述 ()
+ * @copyright: Copyright (c) 2019
+ * @company: 昭阳科技
+ * @author:
+ * @version: 2.0
+ * @date: 2019 2019年5月3日 下午8:30:20
+ */
+public class MsUserVo {
+	private ConstomUser constomUser;
+
+	/**
+	 * constomUser
+	 * 
+	 * @return the constomUser
+	 */
+	public ConstomUser getConstomUser() {
+		return constomUser;
+	}
+
+	/**
+	 * @param constomUser
+	 *            the constomUser to set
+	 */
+	public void setConstomUser(ConstomUser constomUser) {
+		this.constomUser = constomUser;
+	}
+
+}
